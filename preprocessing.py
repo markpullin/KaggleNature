c0c1f52bfbcf8a250d0cf6ba35920dc200fd0518
Mark Pullin <Mark Pullin>
2017-02-27 20:56:46 +0100

    Add function to crop picture

--- preprocessing.py
+++ preprocessing.py
@@ -2,9 +2,13 @@ import os
 
 from PIL import Image
 
+import numpy as np
+
 
 def load_all_images_from_folder(folder, n_max):
     list_of_files = os.listdir(folder)
+    n = np.min((len(list_of_files), n_max))
+    list_of_files = list_of_files[:n]
 
     images = []
     for file_name in list_of_files:
@@ -13,3 +17,20 @@ def load_all_images_from_folder(folder, n_max):
         images.append(im)
 
     return images
+
+
+def take_rectangle_of_image(image, rect_centre, rect_width, rect_height):
+    left_boundary = np.round(rect_centre[0] - rect_width / 2)
+    right_boundary = np.round(rect_centre[0] + rect_width / 2)
+    top_boundary = np.round(rect_centre[1] - rect_height / 2)
+    bottom_boundary = np.round(rect_centre[1] + rect_height / 2)
+    width, height = image.size
+
+    assert left_boundary >= 0
+    assert right_boundary < width
+    assert top_boundary >= 0
+    assert bottom_boundary < height
+
+    crop_area = (left_boundary, top_boundary, right_boundary, bottom_boundary)
+
+    return image.crop(crop_area)
